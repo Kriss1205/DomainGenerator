8b940f12a0be6e9ca8247773d343cb0ae65e8d60
Christopher Morejon <Christophermorejon17@gmail.com>
2023-01-02 05:39:25 +0000

    Finished generator. Single commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,26 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let domainName = [];
+  let pronoun = ["my", "your"];
+  let adj = ["lil", "ol", "huge"];
+  let noun = ["dog", "cat"];
+  let domain = [".com", ".net", ".io", ".gov"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < domain.length; l++) {
+          domainName.push("  " + pronoun[i] + adj[j] + noun[k] + domain[l]);
+        }
+      }
+    }
+  }
+  console.log(domainName);
+
+  // DIDN'T WORK WITH CODE BELOW!!!!
+
+  // document.getElementById("domain.results").innerHTML = domainName;
+  // console.log("Any string");
 };
